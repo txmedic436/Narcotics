eb165fbaf5e3851a11e0a7743d0d4380c89cb2dd
Chris Colpitts <ccolpitts1@yahoo.com>
2021-07-26 15:51:28 -0500

    Initial Commit

---
+++ Narcotics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Narcotics
+//
+//  Created by Christopher Colpitts on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Narcotics/NarcoticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarcoticsApp.swift
+//  Narcotics
+//
+//  Created by Christopher Colpitts on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct NarcoticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
